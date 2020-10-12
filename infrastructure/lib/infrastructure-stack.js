bd267f62d8a1b395cedfb8ecf8978769757fb334
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-10-12 22:06:14 +0100

    Implimented base infrastructure

--- infrastructure/lib/infrastructure-stack.js
+++ infrastructure/lib/infrastructure-stack.js
@@ -1,7 +1,7 @@
-const sns = require('@aws-cdk/aws-sns');
-const subs = require('@aws-cdk/aws-sns-subscriptions');
-const sqs = require('@aws-cdk/aws-sqs');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
 const cdk = require('@aws-cdk/core');
+const apigateway = require('@aws-cdk/aws-apigateway');
+const lambda = require('@aws-cdk/aws-lambda');
 
 class InfrastructureStack extends cdk.Stack {
   /**
@@ -12,14 +12,31 @@ class InfrastructureStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'InfrastructureTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+    // Create a dynamodb table for the resource
+    let iconsTable = new dynamodb.Table(
+      this, "iconsTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      readCapacity: 2,
+      writeCapacity: 2,
+      billingMode: dynamodb.BillingMode.PROVISIONED
+    })
+       var api = new apigateway.RestApi(this, 'APIAllSorts', {
+         restApiName: 'APIAllSorts',
+         defaultCorsPreflightOptions: {
+             allowOrigins: ["*"],
+             allowMethods: ["GET", "POST", "OPTIONS"]
+         }
+     })
+     var getIconsResource = api.root.addResource('icons')
+     const getIcons = new lambda.Function(this, "getIcons", {
+         timeout: cdk.Duration.seconds(10),
+         runtime: lambda.Runtime.NODEJS_12_X,
+         handler: "app.handler",
+         code: lambda.Code.fromAsset('../backend/lambdas/getIcons/'),
+     });
+     var getIconsIntegration = new apigateway.LambdaIntegration(getIcons, { proxy: true })
+     getIconsResource.addMethod('GET', getIconsIntegration)
+     }
 }
 
 module.exports = { InfrastructureStack }
