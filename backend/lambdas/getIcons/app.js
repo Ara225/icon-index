86696e341accfb63238cd0435128896d5f29b426
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-10-14 00:08:00 +0100

    feat: Move to fuse.js for icon search
    DynamoDB will run up a cost due to the way filtering works (get
    everything then filter) Runing fuse over an local JSON file is cheaper
    and better.

--- backend/lambdas/getIcons/app.js
+++ backend/lambdas/getIcons/app.js
@@ -1,13 +1,42 @@
-const AWS = require("aws-sdk");
-// keywords frameworks fuzzyMatch iconStyle
-module.exports.handler = async function (event, context) {
-    event.queryStringParameters
-    if (process.env.LAMBDA_TASK_ROOT) {
-        var dynamodb = new AWS.DynamoDB();
-    }
-    else {
-        console.log("Detected that function is being executed outside the lambda env. Setting DynamoDB endpoint to local container.")
-        var dynamodb = new AWS.DynamoDB({ endpoint: "http://localhost:8000"});
-    }
-}
+const Fuse = require("fuse.js");
 
+module.exports.handler = function (event, context) {
+    let objectToReturn = {
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,PUT,GET,DELETE'
+        },
+        "statusCode": 200,
+        "body": ""
+    };
+    
+    if (!event.queryStringParameters.keywords) {
+        objectToReturn.statusCode = 500;
+        objectToReturn.body = JSON.stringify({"error": "No keywords supplied"})
+        return objectToReturn;
+    }
+    else if (!isNaN(event.queryStringParameters.frameworkID) && event.queryStringParameters.frameworkID != 0) {
+        objectToReturn.statusCode = 500;
+        objectToReturn.body = JSON.stringify({"error": "Invalid framework ID supplied"})
+        return objectToReturn;
+    }
+    let list = require("./icons.json");
+    const options = {
+        includeScore: true,
+        shouldSort: true,
+        threshold: 0.2,
+        keys: ['id'],
+        ignoreLocation: true
+    };
+
+    const fuse = new Fuse(list, options);
+
+    let result = fuse.search(event.queryStringParameters.keywords);
+    if (event.queryStringParameters.frameworkID != undefined) {
+        result = result.filter((value) => {return value.item.frameworkID == event.queryStringParameters.frameworkID;});
+    }
+    objectToReturn.body = JSON.stringify(result);
+    return objectToReturn;
+}
+console.log(module.exports.handler({queryStringParameters: { keywords: "font", frameworkID: 0 }}))
