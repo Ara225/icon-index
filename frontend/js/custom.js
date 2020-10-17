dd94825ff58c5f1f7b89ce904c959b5eae80c8a4
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-10-17 09:48:55 +0100

    feat: Added fuctions to submit form via JS

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -29,3 +29,83 @@ function copyToClipboard(event) {
     document.execCommand("copy");
     event.target.innerText = "Copied!";
 } 
+
+/**
+ * Parses a collection of HTML elements into JSON. Each element has it's ID as the key and the content of it's value 
+ * attribute as the value. 
+ * @param {HTMLCollection} form Collection of objects to parse in to JSON
+ */
+function formToJSON(form) {
+    var data = {}
+    for (var i = 0; i < form.length; i++) {
+        // This allows us to skip fields in the input if we need to
+        console.log(form[i])
+        if (form[i].id == "" || form[i].tagName == "BUTTON") {
+            continue
+        }
+        else {
+            if (form[i].value == "" && form[i].required) {
+                alert("Unable to validate form. Please ensure all required fields are completed")
+                return false;
+            }
+            else {
+                if (form[i].type == "checkbox") {
+                    // Code to handle groups of checkboxes as one
+                    if (form[i].name) {
+                        let selected = document.querySelectorAll('input[name="'+ form[i].name + '"]:checked');
+                        let values = [];
+                        for (let index = 0; index < selected.length; index++) {
+                            values.push(selected[index].value);
+                        }
+                        data[form[i].name] = values.toString();
+                    }
+                    else {
+                        data[form[i].id] = form[i].checked;
+                    }
+                }
+                else if (form[i].type == "radio") {
+                    // Radio buttons are grouped by name and only one can be selected at any time. Requires slightly different approach
+                    let selectedRadioButton = document.querySelector('input[name="'+ form[i].name + '"]:checked').id;
+                    if (!selectedRadioButton && form[i].required) {
+                        alert("Unable to validate form. Please ensure all required fields are completed")
+                        return false;
+                    }
+                    data[form[i].name] = selectedRadioButton;
+                }
+                else {
+                    data[form[i].id] = form[i].value;
+                }
+            }
+        }
+    }
+    return data;
+}
+
+/**
+ * Handles form submission
+ * @param {Object} event 
+ */
+async function onFormSubmit(event) {
+    try {
+        var form = formToJSON(event.target);
+        console.log(form)
+        if (!form) {
+            return false;
+        }
+        var res = await fetch("https://example.com/api", {method: "POST", body: JSON.stringify(form)});
+        var jsonResult = await res.json();
+        if (res.status != 200) {
+            alert("The endpoint returned a status code other than 200")
+            console.log(jsonResult);
+            return false;
+        }
+        alert("Success")
+        return true;
+    }
+    catch(e) {
+        alert("Error occurred")
+        console.log(e);
+        console.log(jsonResult);
+        return false;
+    }
+}
